2350ac9b08502d99b67af693bef753ab029853f6
Prashant Bhadane <pkbhadane545@gmail.com>
2026-02-19 15:31:30 +0530

    Refactor user route imports and add additional user account management endpoints

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,19 @@
 import express from "express";
-import { refreshAccessToken, userLogin, userLogout, userRegister } from "../controllers/user.controller.js";
+import {
+  changePassword,
+  getChannelProfile,
+  getWatchHistory,
+  refreshAccessToken,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
+  userLogin,
+  userLogout,
+  userRegister,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const userRouter = express.Router();
 
 userRouter.route("/register").post(
@@ -15,12 +27,23 @@ userRouter.route("/register").post(
   userRegister,
 ); // using this syntax we can use multiple methods i.e-put,get etc. for same path
 
-userRouter.route("/login").post(userLogin)
+userRouter.route("/login").post(userLogin);
+userRouter.route("/refresh").post(refreshAccessToken);
 
-userRouter.route("/logout").post(verifyJWT, userLogout)
-
-userRouter.route("/refresh").post(refreshAccessToken)
-
+// secured routes
+userRouter.route("/logout").post(verifyJWT, userLogout);
+userRouter.route("/change-password").post(verifyJWT, changePassword);
+userRouter.route("/update-account").patch(verifyJWT, updateAccountDetails);
+userRouter
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+userRouter
+  .route("/coverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+userRouter
+  .route("/channel-profile/:username")
+  .get(verifyJWT, getChannelProfile);
+userRouter.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 // router.post("/register", userRegister) // common and simple syntax
 
